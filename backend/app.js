a9fd5df35e737db3d593b57c5029c47c8069a6f4
geoffreykemboi <geoffreyk126@gmail.com>
2025-07-01 18:01:55 +0300

    Changes

--- backend/app.js
+++ backend/app.js
@@ -44,7 +44,14 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: allowedOrigins,
+  origin: function(origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error('Not allowed by CORS'), false);
+    }
+  },
   credentials: true,
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   allowedHeaders: "Origin,X-Requested-With,Content-Type,Accept,Authorization"
